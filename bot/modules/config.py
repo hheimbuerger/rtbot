b8d521e192047a405d4a91e4b7589c0fe0dcc5e1
Henrik Heimbuerger <henrik@heimbuerger.de>
2006-04-05 15:40:08 +0000

    Config: * updated default channel to #RollingThunder.development + added webservice parameters

--- bot/modules/config.py
+++ bot/modules/config.py
@@ -1,7 +1,18 @@
 class Settings:
+    # The IRC server to connect to.
     server = "de.quakenet.org"
+
+    # The port used by the IRC server. (The default port is 6667, but some server hosts block this port.)
     port = 6668
-    channel = "#RollingThunder.test"
+
+    # The channel to connect to.
+    channel = "#RollingThunder.development"
+
+    # The nickname, username and realname to use (for WHOIS).
     nickname = "RTBotTest"
     username = "rtbottest"
     realname = "RTBotTest"
+
+    # The host and port of the webservice or "None" to deactivate the webservice.
+    webservice_host = None      #"tufer.de"
+    webservice_port = None      #8000
