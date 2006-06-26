92bcab580e2a6998fea0eabc2a38cd2088d602a5
terralthra <terralthra@users.noreply.github.com>
2006-06-26 10:50:13 +0000

    I think I've fixed it. SummoningPlugin.py changed.

--- bot/plugins/SummoningPlugin.py
+++ bot/plugins/SummoningPlugin.py
@@ -7,7 +7,7 @@ class SummoningPlugin:
 
     def onChannelMessage(self, irclib, source, message):
         if(message[:len(self.summonCommand)] == self.summonCommand):
-            target = message[len(self.summonCommand)+1]
+            target = message[len(self.summonCommand)+1:]
             if(target in irclib.getUserList().getPureList()):
                 irclib.sendPrivateNotice(target, "You have been summoned by %s!" % (source))
                 irclib.sendChannelEmote("does a ritual dance.")
@@ -17,12 +17,12 @@ class SummoningPlugin:
                 irclib.sendChannelMessage("Oh, great %s, from the depths of idling, we summon thee!" % (target))
                 irclib.sendChannelEmote("fizzles. :(")
         if(message[:len(self.banishCommand)] == self.banishCommand):
-            target = message[len(self.summonCommand):]
+            target = message[len(self.banishCommand)+1:]
             if(target in irclib.getUserList().getPureList()):
                 irclib.sendPrivateNotice(target, "You feel your soul exorcized by %s" %(source))
                 irclib.sendChannelEmote("burns some bat guano.")
                 irclib.sendChannelMessage("Oh, evil %s, from this channel we banish thee!" % (target))
-                irclib.sendRawMsg("/KICK %s %s" % (irclib.channel, target))
+                irclib.sendRawMsg("/kick %s %s" % (irclib.channel, target))
             else:
                 irclib.sendChannelEmote("burns some bat guano.")
                 irclib.sendChannelMessage("Oh, evil %s, from this channel we banish thee!" % (target))
