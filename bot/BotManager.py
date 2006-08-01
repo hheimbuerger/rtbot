cf011b27898fe4ee693436ede7c200d5aa21b00a
Henrik Heimbuerger <henrik@heimbuerger.de>
2006-08-01 18:35:54 +0000

    BotManager & daemonize: * start and stop no longer make the console call 'hang' when called from the webapp now (for whatever reason that was, something with stdout/err redirection) * restart does no longer work, no clue why. The process is stopped, but just not restarted. Can't notice anything going wrong. + stop is now done by calling SIGTERM, which is catched by the bot and leads to a graceful shutdown. BotManager: + Now keeps a status flag for the webapp. * Cleaned up code. + Added webservice management.

--- bot/BotManager.py
+++ bot/BotManager.py
@@ -3,173 +3,182 @@ from modules import IrcLib, RTBot, Log, config, PluginInterface, util, WebServic
 from lib import daemonize
 
 class BotManager:
-    pluginsDirectory = "plugins"
-    timeoutBeforeReconnecting = 60
-    
-    def __init__(self):
-        self.status = "Loaded."
-        logging.info("----------------------------------------------------------------------")
-        logging.info("Starting up RTBot...")
-        self.settings = config.Settings()
+	pluginsDirectory = "plugins"
+	timeoutBeforeReconnecting = 60
 
-    def controller_IRCLibrary(self, action):
-        if(action == "start"):
-            logging.info("Loading IRC library...")
-            self.irclib = IrcLib.LowlevelIrcLib()
-            self.irclib.connect(self.settings.server, self.settings.port, self.settings.nickname, self.settings.username, self.settings.realname)
-        elif(action == "stop"):
-            logging.info("Disconnecting from IRC...")
-            try:
-                self.irclib.disconnect()
-            except Exception, exc:
-                logging.exception("Exception during irclib.disconnect()")
-            self.irclib = None
-        else:
-            raise Exception("Unsupported action sent to controller_IRCLibrary: %s" % action)
+	# -----------------------
+	# PRIVATE
+	# -----------------------
+	
+	def __init__(self):
+		self.status = "Loaded."
+		logging.info("----------------------------------------------------------------------")
+		logging.info("Starting up RTBot...")
+		self.settings = config.Settings()
 
-    def controller_PluginInterface(self, action):
-        if(action == "start"):
-            logging.info("Loading PluginInterface...")
-            self.pluginInterface = PluginInterface.PluginInterface(self.pluginsDirectory)
-        elif(action == "stop"):
-            logging.info("Unloading PluginInterface...")
-            try:
-                self.pluginInterface.dispose()
-            except Exception, exc:
-                logging.exception("Exception during pluginInterface.dispose()")
-            self.pluginInterface = None
-        else:
-            raise Exception("Unsupported action sent to controller_PluginInterface: %s" % action)
+	def controller_IRCLibrary(self, action):
+		if(action == "start"):
+			logging.info("Loading IRC library...")
+			self.irclib = IrcLib.LowlevelIrcLib()
+			self.irclib.connect(self.settings.server, self.settings.port, self.settings.nickname, self.settings.username, self.settings.realname)
+		elif(action == "stop"):
+			logging.info("Disconnecting from IRC...")
+			try:
+				self.irclib.disconnect()
+			except Exception, exc:
+				logging.exception("Exception during irclib.disconnect()")
+			self.irclib = None
+		else:
+			raise Exception("Unsupported action sent to controller_IRCLibrary: %s" % action)
 
-    def controller_BotCore(self, action):
-        if(action == "start"):
-            logging.info("Loading core bot code...")
-            self.rtbot = RTBot.RTBot(self.irclib, self.settings.channel, self.pluginInterface)
-            self.irclib.registerEventTarget(self.rtbot)
-            self.pluginInterface.registerInformTarget(self.rtbot)
-        elif(action == "stop"):
-            logging.info("Unloading core bot code...")
-            try:
-                self.rtbot.dispose()
-            except Exception, exc:
-                logging.exception("Exception during rtbot.dispose()")
-            self.rtbot = None
-        else:
-            raise Exception("Unsupported action sent to controller_BotCore: %s" % action)
+	def controller_PluginInterface(self, action):
+		if(action == "start"):
+			logging.info("Loading PluginInterface...")
+			self.pluginInterface = PluginInterface.PluginInterface(self.pluginsDirectory)
+		elif(action == "stop"):
+			logging.info("Unloading PluginInterface...")
+			try:
+				self.pluginInterface.dispose()
+			except Exception, exc:
+				logging.exception("Exception during pluginInterface.dispose()")
+			self.pluginInterface = None
+		else:
+			raise Exception("Unsupported action sent to controller_PluginInterface: %s" % action)
 
-    def controller_Plugins(self, action):
-        if(action == "start"):
-            logging.info("Loading plugins...")
-            self.pluginInterface.updatePlugins(False)
-        elif(action == "stop"):
-            pass
-        else:
-            raise Exception("Unsupported action sent to controller_Plugins: %s" % action)
+	def controller_BotCore(self, action):
+		if(action == "start"):
+			logging.info("Loading core bot code...")
+			self.rtbot = RTBot.RTBot(self.irclib, self.settings.channel, self.pluginInterface)
+			self.irclib.registerEventTarget(self.rtbot)
+			self.pluginInterface.registerInformTarget(self.rtbot)
+		elif(action == "stop"):
+			logging.info("Unloading core bot code...")
+			try:
+				self.rtbot.dispose()
+			except Exception, exc:
+				logging.exception("Exception during rtbot.dispose()")
+			self.rtbot = None
+		else:
+			raise Exception("Unsupported action sent to controller_BotCore: %s" % action)
 
-    def controller_ModificationsTimer(self, action):
-        if(action == "start"):
-            logging.info("Starting to check for changed plugins every X seconds...")
-            self.modificationsTimerLock = threading.RLock()
-            self.startModificationsTimer()
-        elif(action == "stop"):
-            logging.info("Stopping checking for changed plugins...")
-            self.modificationsTimer.cancel()
-            self.modificationsTimer = None
-        else:
-            raise Exception("Unsupported action sent to controller_ModificationsTimer: %s" % action)
+	def controller_Plugins(self, action):
+		if(action == "start"):
+			logging.info("Loading plugins...")
+			self.pluginInterface.updatePlugins(False)
+		elif(action == "stop"):
+			pass
+		else:
+			raise Exception("Unsupported action sent to controller_Plugins: %s" % action)
 
-    def controller_WebService(self, action):
-        if(action == "start"):
-            if(self.settings.webservice_host and self.settings.webservice_port):
-                logging.info("Starting WebService...")
-                self.webservice = WebService.WebService(self, self.settings.webservice_host, self.settings.webservice_port)
-                self.webservice.start()
-            else:
-                logging.info("WebService deactivated...")
-        elif(action == "stop"):
-            logging.info("STOPPING WEBSERVICE NOT YET IMPLEMENTED!")
-            self.webservice = None
-        else:
-            raise("NO ACTION SPECIFIED")
+	def controller_ModificationsTimer(self, action):
+		if(action == "start"):
+			logging.info("Starting to check for changed plugins every X seconds...")
+			self.modificationsTimerLock = threading.RLock()
+			self.startModificationsTimer()
+		elif(action == "stop"):
+			logging.info("Stopping checking for changed plugins...")
+			self.modificationsTimer.cancel()
+			self.modificationsTimer = None
+		else:
+			raise Exception("Unsupported action sent to controller_ModificationsTimer: %s" % action)
 
-    def startup(self):
-        self.controller_IRCLibrary("start")
-        self.controller_PluginInterface("start")
-        self.controller_BotCore("start")
-        self.controller_Plugins("start")
-        self.controller_ModificationsTimer("start")
+	def controller_WebService(self, action):
+		if(action == "start"):
+			if(self.settings.webservice_host and self.settings.webservice_port):
+				logging.info("Starting WebService...")
+				self.webservice = WebService.WebService(self, self.settings.webservice_host, self.settings.webservice_port)
+				self.webservice.start()
+			else:
+				logging.info("WebService deactivated...")
+		elif(action == "stop"):
+			logging.info("STOPPING WEBSERVICE NOT YET IMPLEMENTED!")
+			self.webservice = None
+		else:
+			raise("NO ACTION SPECIFIED")
 
-    @util.withMemberLock("modificationsTimerLock")
-    def shutdown(self):
-        self.controller_ModificationsTimer("stop")
-        self.controller_PluginInterface("stop")
-        self.controller_BotCore("stop")
-        self.controller_IRCLibrary("stop")
+	def startup(self):
+		self.controller_IRCLibrary("start")
+		self.controller_PluginInterface("start")
+		self.controller_BotCore("start")
+		self.controller_Plugins("start")
+		self.controller_ModificationsTimer("start")
 
-    def run(self):
-        self.status = "Launched."
-        self.controller_WebService("start")
-        
-        logging.debug( "Entering main BotManager loop...")        
+	@util.withMemberLock("modificationsTimerLock")
+	def shutdown(self):
+		self.controller_ModificationsTimer("stop")
+		self.controller_PluginInterface("stop")
+		self.controller_BotCore("stop")
+		self.controller_IRCLibrary("stop")
 
-        while(True):
-            logging.info("Trying to initialise bot...")
-            try:
-                self.startup()
-            except IrcLib.ServerConnectionError, x:
-                logging.exception("Exception: ServerConnectionError during connect!")
-            else:
-                self.status = "Connected."
-                try:
-                    self.irclib.receiveDataLooped()        # receiveDataLooped() won't return unless the user requested a quit
-                    break
-                except IrcLib.ServerConnectionError, x:
-                    logging.exception("Exception: ServerConnectionError in BotManager.run()")
-                    # will try to reconnect
-                except IrcLib.IrcError, x:
-                    logging.exception("Exception: IrcError in BotManager.run()")
-                    # will try to reconnect
-                except Exception, x:
-                    logging.exception("Exception: ")
-                    self.irclib.sendChannelEmote("is confused and runs away panicking")
-                    # will not try to reconnect
-                    break
+	def startModificationsTimer(self):
+		self.modificationsTimer = threading.Timer(10.0, self.checkForModifications)
+		self.modificationsTimer.start()
 
-            # wait x seconds before reconnecting
-            self.status = "Disconnected."
-            self.shutdown()
-            time.sleep(self.timeoutBeforeReconnecting)
+	@util.withMemberLock("modificationsTimerLock", False) # If we can't lock, then we're shutting down - do nothing
+	def checkForModifications(self):
+		self.pluginInterface.updatePlugins()
+		
+		# Restart the timer
+		self.startModificationsTimer()
 
-        self.status = "Shutting down."
-        logging.info("BotManager.run(): Destructing BotManager...")
-        self.shutdown()
-        logging.info("Thank you for using RTBot! 'yb")
 
-        self.controller_WebService("stop")
-        self.status = "Shut down."
+		
+	# -----------------------
+	# PUBLIC
+	# -----------------------
 
-    def startModificationsTimer(self):
-        self.modificationsTimer = threading.Timer(10.0, self.checkForModifications)
-        self.modificationsTimer.start()
+	def getState(self):
+		return(self.status)
 
-    @util.withMemberLock("modificationsTimerLock", False) # If we can't lock, then we're shutting down - do nothing
-    def checkForModifications(self):
-        self.pluginInterface.updatePlugins()
-        
-        # Restart the timer
-        self.startModificationsTimer()
+	def run(self):
+		self.status = "Launched."
+		self.controller_WebService("start")
+		
+		logging.debug( "Entering main BotManager loop...")		
 
-    def getState(self):
-        return(self.status)
+		while(True):
+			logging.info("Trying to initialise bot...")
+			try:
+				self.startup()
+			except IrcLib.ServerConnectionError, x:
+				logging.exception("Exception: ServerConnectionError during connect!")
+			else:
+				self.status = "Connected."
+				try:
+					self.irclib.receiveDataLooped()		# receiveDataLooped() won't return unless the user requested a quit
+					break
+				except IrcLib.ServerConnectionError, x:
+					logging.exception("Exception: ServerConnectionError in BotManager.run()")
+					# will try to reconnect
+				except IrcLib.IrcError, x:
+					logging.exception("Exception: IrcError in BotManager.run()")
+					# will try to reconnect
+				except Exception, x:
+					logging.exception("Exception: ")
+					self.irclib.sendChannelEmote("is confused and runs away panicking")
+					# will not try to reconnect
+					break
 
+			# wait x seconds before reconnecting
+			self.status = "Disconnected."
+			self.shutdown()
+			time.sleep(self.timeoutBeforeReconnecting)
+
+		self.status = "Shutting down."
+		logging.info("BotManager.run(): Destructing BotManager...")
+		self.shutdown()
+		logging.info("Thank you for using RTBot! 'yb")
+
+		self.controller_WebService("stop")
+		self.status = "Shut down."
 
 
 
 
 if __name__ == "__main__":
-    if(os.name == 'posix'):
-        daemonize.startstop(stdout='/tmp/rtbot.log',
-                            stderr='/tmp/rtbot_err.log',
-                            pidfile='/tmp/rtbot.pid')
-    botManager = BotManager()
-    botManager.run()
+	if(os.name == 'posix'):
+		daemonize.startstop(stdout='logs/rtbot.log',
+			stderr='logs/rtbot_err.log',
+			pidfile='rtbot.pid')
+	botManager = BotManager()
+	botManager.run()
--- bot/lib/daemonize.py
+++ bot/lib/daemonize.py
@@ -19,10 +19,51 @@
       
       http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66012
 """
-import os, sys, time
-from signal import SIGTERM
 
-def deamonize(stdout='/dev/null', stderr=None, stdin='/dev/null',
+# A daemon process is usually defined as a background process that does not belong to a terminal session. Many
+# system services are performed by daemons; network services, printing etc.
+
+# Simply invoking a program in the background isn't really adequate for these long-running programs; that does
+# not correctly detach the process from the terminal session that started it. Also, the conventional way of
+# starting daemons is simply to issue the command manually or from an rc script; the daemon is expected to put
+# itself into the background.
+
+# Here are the steps to become a daemon:
+
+#   1. fork() so the parent can exit, this returns control to the command line or shell invoking your program.
+#      This step is required so that the new process is guaranteed not to be a process group leader. The next
+#      step, setsid(), fails if you're a process group leader.
+#   2. setsid() to become a process group and session group leader. Since a controlling terminal is associated
+#      with a session, and this new session has not yet acquired a controlling terminal our process now has no
+#      controlling terminal, which is a Good Thing for daemons.
+#   3. fork() again so the parent, (the session group leader), can exit. This means that we, as a non-session
+#      group leader, can never regain a controlling terminal.
+#   4. chdir("/") to ensure that our process doesn't keep any directory in use. Failure to do this could make
+#      it so that an administrator couldn't unmount a filesystem, because it was our current directory.
+#      [Equivalently, we could change to any directory containing files important to the daemon's operation.]
+#   5. umask(0) so that we have complete control over the permissions of anything we write. We don't know what
+#      umask we may have inherited. [This step is optional]
+#   6. close() fds 0, 1, and 2. This releases the standard in, out, and error we inherited from our parent
+#      process. We have no way of knowing where these fds might have been redirected to. Note that many daemons
+#      use sysconf() to determine the limit _SC_OPEN_MAX. _SC_OPEN_MAX tells you the maximun open files/process.
+#      Then in a loop, the daemon can close all possible file descriptors. You have to decide if you need to do
+#      this or not. If you think that there might be file-descriptors open you should close them, since there's
+#      a limit on number of concurrent file descriptors.
+#   7. Establish new open descriptors for stdin, stdout and stderr. Even if you don't plan to use them, it is
+#      still a good idea to have them open. The precise handling of these is a matter of taste; if you have a
+#      logfile, for example, you might wish to open it as stdout or stderr, and open `/dev/null' as stdin;
+#      alternatively, you could open `/dev/console' as stderr and/or stdout, and `/dev/null' as stdin, or any
+#      other combination that makes sense for your particular daemon. 
+
+import os, sys, time, signal
+
+usedSignal = signal.SIGTERM
+
+def signalHandler(signum, frame):
+    print 'Signal handler called with signal', signum
+    raise KeyboardInterrupt, "SIGNAL received!"
+
+def deamonize(stdout='/dev/null', stderr='/dev/null', stdin='/dev/null',
               pidfile=None, startmsg = 'started with pid %s' ):
     """
         This forks the current process into a daemon.
@@ -34,24 +75,32 @@ def deamonize(stdout='/dev/null', stderr=None, stdin='/dev/null',
         if it shares a file with stdout then interleaved output
         may not appear in the order that you expect.
     """
+    
+    # set a handler for signal
+    signal.signal(usedSignal, signalHandler)
+
     # Do first fork.
     try: 
         pid = os.fork() 
         if pid > 0: sys.exit(0) # Exit first parent.
+        print "fork #1 succeeded"
     except OSError, e: 
+        print "fork #1 failed: (%d) %s\n" % (e.errno, e.strerror)
         sys.stderr.write("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror))
         sys.exit(1)
 
     # Decouple from parent environment.
     #os.chdir("/") 
     os.umask(0) 
-    os.setsid() 
+    os.setsid()
     
     # Do second fork.
     try: 
         pid = os.fork() 
+        print "fork #2 succeeded"
         if pid > 0: sys.exit(0) # Exit second parent.
     except OSError, e: 
+        print "fork #2 failed: (%d) %s\n" % (e.errno, e.strerror)
         sys.stderr.write("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror))
         sys.exit(1)
     
@@ -64,48 +113,79 @@ def deamonize(stdout='/dev/null', stderr=None, stdin='/dev/null',
     sys.stderr.write("\n%s\n" % startmsg % pid)
     sys.stderr.flush()
     if pidfile: file(pidfile,'w+').write("%s\n" % pid)
+    print "opened file descriptors"
     
     # Redirect standard file descriptors.
     os.dup2(si.fileno(), sys.stdin.fileno())
-    #os.dup2(so.fileno(), sys.stdout.fileno())
-    #os.dup2(se.fileno(), sys.stderr.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
+
+
 
 def startstop(stdout='/dev/null', stderr=None, stdin='/dev/null',
               pidfile='pid.txt', startmsg = 'started with pid %s' ):
+    # print usage if no argument has been supplied
     if len(sys.argv) > 1:
+        # detect the action
         action = sys.argv[1]
+        print "action: %s" % (action)
+        
+        # read the PID
         try:
             pf  = file(pidfile,'r')
             pid = int(pf.read().strip())
             pf.close()
+            print "Found the pid: %i" % (pid)
         except IOError:
             pid = None
+            print "pid not found, not necessarily an error."
+
+        # STOP / RESTART
         if 'stop' == action or 'restart' == action:
+            # error message if no PID
             if not pid:
                 mess = "Could not stop, pid file '%s' missing.\n"
                 sys.stderr.write(mess % pidfile)
                 sys.exit(1)
+
+            # try to kill the old process until it's dead
+            print "Trying to kill the old process and then waiting until it's gone."
             try:
-               while 1:
-                   os.kill(pid,SIGTERM)
+                os.kill(pid, usedSignal)
+                while 1:
+                   os.kill(pid, signal.SIGCONT)
                    time.sleep(1)
+
+            # this is not an exception, it's the expected case that the process is gone
             except OSError, err:
+               print "The process is gone now."
                err = str(err)
                if err.find("No such process") > 0:
+                   print "Couldn't find the process, this is expected and not an error."
+                   # delete the pid file
                    os.remove(pidfile)
+                   # if STOP, then we're done here
                    if 'stop' == action:
                        sys.exit(0)
+                   # if RESTART, we need to do the START-stuff now
                    action = 'start'
+                   print "new action: %s" % (action)
                    pid = None
                else:
+                   print "Found process, this is an error, exiting."
                    print str(err)
                    sys.exit(1)
         if 'start' == action:
+            print "Starting to handle the 'start' action."
             if pid:
                 mess = "Start aborded since pid file '%s' exists.\n"
+                print mess % pidfile
                 sys.stderr.write(mess % pidfile)
                 sys.exit(1)
+            # run daemonize()
+            print "Running daemonize()..."
             deamonize(stdout,stderr,stdin,pidfile,startmsg)
+            print "Ran daemonize()."
             return
     print "usage: %s start|stop|restart" % sys.argv[0]
     sys.exit(2)
